14f43462ddde301ecb8cf554841470e2c5360801
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-03-27 00:50:50 +0000

    Lecturer

---
+++ src/uk/co/fuuzetsu/cm20215cwk2/Lecturer.java
@@ -0,0 +1,50 @@
+package uk.co.fuuzetsu.cm20215cwk2;
+
+import fj.F;
+import fj.Unit;
+import fj.data.Either;
+
+public class Lecturer extends Employee {
+
+    public Lecturer(final String jobTitle, final String name,
+                    final Integer id, final Salary salary,
+                    final Address address, final PhoneNumber phoneNumber) {
+        super("Lecturer", name, id, new Salary(20000d), address, phoneNumber);
+    }
+
+    @Override
+    public Either<IO<Either<String, Money>>, Money> calculateWage() {
+
+        F<Unit, Either<String, Money>> f =
+            new F<Unit, Either<String, Money>>() {
+
+            @Override
+            public Either<String, Money> f(Unit u) {
+                Either<String, Double> c =
+                  Util.getPositiveDouble
+                    ("Enter number of consultancy hours.");
+
+                if (c.isLeft()) {
+                    return Either.left(c.left().value());
+                }
+
+                Either<String, Double> p =
+                  Util.getPositiveDouble
+                    ("Enter yearly performance pay.");
+
+                if (p.isLeft()) {
+                    return Either.left(p.left().value());
+                }
+
+                Money cp = new Money(c.right().value() * 20d);
+                Money pp = new Salary(new Money(p.right().value())).getMonthly();
+
+                return Either.right(getSalary().getMonthly().add(cp).add(pp));
+            }
+
+
+        };
+
+        return Either.left(new IO<>(f));
+    }
+}
