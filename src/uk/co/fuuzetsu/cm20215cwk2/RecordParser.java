d54bec2673defea859813d9f187d7fc97305ecd5
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-03-28 12:02:49 +0000

    License code

--- src/uk/co/fuuzetsu/cm20215cwk2/RecordParser.java
+++ src/uk/co/fuuzetsu/cm20215cwk2/RecordParser.java
@@ -1,3 +1,18 @@
+/* This file is part of mk440-cm20215-cwk2.
+ *
+ * mk440-cm20215-cwk2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mk440-cm20215-cwk2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mk440-cm20215-cwk2.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.co.fuuzetsu.cm20215cwk2;
 
 import fj.data.Either;
